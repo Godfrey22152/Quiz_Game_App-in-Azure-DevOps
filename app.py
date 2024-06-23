58def2f0eddaad51b2ca30c90ad17d9f47123c27
GODFREY IFEANYI <90422561+Godfrey22152@users.noreply.github.com>
2024-06-23 14:43:14 +0100

    Updated app.py

--- app.py
+++ app.py
@@ -1,40 +1,77 @@
 import os
 import re
-import random 
+import random
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, redirect, url_for, session, flash, abort
-from flask_session import Session, MongoDBSessionInterface
-from pymongo import MongoClient
+from flask.sessions import SessionInterface, SessionMixin
+from pymongo import MongoClient, errors
 from bson import ObjectId
 from datetime import datetime, timedelta
 
+class MyMongoDBSession(dict, SessionMixin):
+    def __init__(self, initial=None, sid=None):
+        super().__init__(initial or {})
+        self.sid = sid
 
-class MyMongoDBSessionInterface(MongoDBSessionInterface):
+class MyMongoDBSessionInterface(SessionInterface):
     def __init__(self, client, db, collection, key_prefix):
         self.store = client[db][collection]
         self.key_prefix = key_prefix
 
-    def get_store_id(self, session):
-        return self.key_prefix + session['f_sid']
+    def open_session(self, app, request):
+        sid = request.cookies.get(app.config['SESSION_COOKIE_NAME'])
+        if not sid:
+            sid = os.urandom(24).hex()
+            return MyMongoDBSession(sid=sid)
 
-    def get_mutable_dict(self, session):
-        # Adjust as needed based on your session structure
-        return dict(session)
+        session_data = self.store.find_one({'_id': self.key_prefix + sid})
+        if session_data and 'data' in session_data:
+            return MyMongoDBSession(session_data['data'], sid=sid)
+        return MyMongoDBSession(sid=sid)
 
     def save_session(self, app, session, response):
-        store_id = self.get_store_id(session)
-        data = self.get_mutable_dict(session)
-        expiration = datetime.utcnow() + timedelta(minutes=app.config['PERMANENT_SESSION_LIFETIME'])
-        self.store.update_one({'_id': store_id}, {'$set': {'data': data, 'expiration': expiration}}, upsert=True)
+        domain = self.get_cookie_domain(app)
+        if not session:
+            self.store.delete_one({'_id': self.key_prefix + session.sid})
+            response.delete_cookie(app.config['SESSION_COOKIE_NAME'], domain=domain)
+            return
 
+        expiration = datetime.utcnow() + app.permanent_session_lifetime
+        self.store.update_one(
+            {'_id': self.key_prefix + session.sid},
+            {'$set': {'data': dict(session), 'expiration': expiration}},
+            upsert=True
+        )
+        response.set_cookie(
+            app.config['SESSION_COOKIE_NAME'],
+            session.sid,
+            expires=expiration,
+            httponly=True,
+            domain=domain
+        )
+
+    def is_null_session(self, session):
+        return not bool(session)
 
 app = Flask(__name__)
+
+# Set the secret key to a random value
 app.secret_key = os.urandom(24)
 
-# Connect to MongoDB
-client = MongoClient('mongodb://mongo:27017/')
-db = client['quiz_app']
+# Load environment variables
+load_dotenv()
+mongo_uri = os.environ.get('MONGO_URI')
+
+# Connect to MongoDB Atlas
+client = MongoClient(mongo_uri)
+client.server_info()  
+print("Connected to MongoDB successfully!")
+
+# Access the "quiz_app" database
+db = client.get_database('quiz_app')
 dumps_collection = db['dumps']
 results_data_collection = db['results']
+
 # Dictionary mapping quiz categories to their collections
 quiz_collections = {
     'devOps': db['devOps_quizzes'],
@@ -51,16 +88,22 @@ quiz_category_to_question_collections = {
     'azure': db['azure_questions']
 }
 
-# Initialize Flask-Session
-app.config['SESSION_TYPE'] = 'mongodb'
+# Initialize Flask-Session with the custom session interface
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
-app.config['SESSION_USE_SIGNER'] = True  # Sign the session cookie for extra security
-app.config['SESSION_KEY_PREFIX'] = 'quiz_app_'  # Replace with your desired prefix
-
-# Use the custom MyMongoDBSessionInterface
 mongo_session = MyMongoDBSessionInterface(client, db='quiz_app', collection='sessions', key_prefix='quiz_app_')
-Session(app)
+app.session_interface = mongo_session  # Set the custom session interface
 
+@app.route('/healthz')
+def healthz():
+    return 'OK', 200
+
+@app.route('/ready')
+def ready():
+    try:
+        client.server_info()  # Force connection on a request.
+        return 'READY', 200
+    except errors.ServerSelectionTimeoutError:
+        return 'NOT READY', 500
 
 @app.route('/')
 @app.route('/welcome')
@@ -110,7 +153,7 @@ def login():
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
-        
+
         account = db.accounts.find_one({'username': username, 'password': password})
 
         if account:
@@ -138,7 +181,7 @@ def logout():
 @app.route('/variety')
 def variety():
     if 'loggedin' in session:
-        msg = request.args.get('msg', '')  
+        msg = request.args.get('msg', '')
         return render_template("variety.html", msg=msg)
 
     return redirect(url_for('login'))
@@ -146,8 +189,18 @@ def variety():
 @app.route('/dumps')
 def dumps():
     dumps = dumps_collection.find()
-    return render_template('dumps.html', dumps=dumps)
+    dump_lists = [[] for _ in range(4)]  # List the 4 lists available in the DB
 
+    for idx, dump in enumerate(dumps):
+        dump_info = {
+            'title': dump.get('title', 'No Title'),
+            'description': dump.get('description', 'No Description'),
+            'num_dumps': dump.get('num_dumps', 'N/A'),
+            'category': dump.get('category', 'general')
+        }
+        dump_lists[idx % 4].append(dump_info)
+
+    return render_template('dumps.html', dump_lists=dump_lists)
 
 @app.route('/select_quiz', methods=['GET'])
 def select_quiz():
@@ -199,7 +252,7 @@ def start_round(quiz_id):
     quiz_collection = quiz_category_to_question_collections.get(selected_quiz)
 
     # Extract the 'questions' field from the document
-    questions = quiz_collection.find_one({}).get('questions')  
+    questions = quiz_collection.find_one({}).get('questions')
 
     total_questions = len(questions)
 
@@ -268,10 +321,10 @@ def results():
     if len(correct_answers) == 0:
         score = 0
     else:
-        score = int((correct_guesses / len(questions)) * 100)  
+        score = int((correct_guesses / len(questions)) * 100)
 
     total_correct = correct_guesses
-    total_wrong = len(questions) - total_correct 
+    total_wrong = len(questions) - total_correct
 
     results_data = []
     for question_num, (question, correct_ans, user_ans) in enumerate(question_answer_pairs, start=1):
@@ -290,7 +343,7 @@ def results():
 
 def check_answers(user_answers, correct_answers, questions):
     if len(user_answers) != len(correct_answers):
-        return 0, []  
+        return 0, []
 
     correct_guesses = sum(1 for user_ans, correct_ans in zip(user_answers, correct_answers) if user_ans == correct_ans)
 
@@ -300,4 +353,3 @@ def check_answers(user_answers, correct_answers, questions):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
